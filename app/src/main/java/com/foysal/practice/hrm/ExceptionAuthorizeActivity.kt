2f03d98b88931740a0330ceddb2adf64bed86690
TatsuSolo <1730150@iub.edu.bd>
2022-11-13 23:41:13 +0600

    home-13/11/22

---
+++ app/src/main/java/com/foysal/practice/hrm/ExceptionAuthorizeActivity.kt
@@ -0,0 +1,11 @@
+package com.foysal.practice.hrm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExceptionAuthorizeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exception_authorize)
+    }
+}
